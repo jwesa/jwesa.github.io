2adcdc567a2922e119443e3645f633a97f4d6809
jwesa <layoshicity@gmail.com>
2022-05-28 15:59:09 +0300

    Heart on hover over image

--- src/components/CatCard/CatCard.js
+++ src/components/CatCard/CatCard.js
@@ -1,10 +1,11 @@
 import "./CatCard.scss";
+import heart from "../../assets/heart.svg";
 
 const CatCard = ({ id, url }) => {
     return (
         <div className="cat-card">
-            <img src={url} alt="Cat img" />
-            {/* <button className="cat-card__button">&hearts;</button> */}
+            <img className="cat-card__image" src={url} alt="Cat img" />
+            <img className="cat-card__like-button" src={heart} alt="heart" />
         </div>
     );
 };
