1c7b86a4d8d75d58ed149d5f55d100c151169229
jwesa <layoshicity@gmail.com>
2022-05-28 20:16:55 +0300

    Endless scroll

--- src/App.js
+++ src/App.js
@@ -4,16 +4,46 @@ import FavoriteCats from "./components/FavoriteCats/FavoriteCats";
 
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchCats } from "./store/reducers/fetchCatsReducer";
+import { useEffect, useState } from "react";
+import { fetchCats, loadMore } from "./store/reducers/fetchCatsReducer";
 
 function App() {
     const dispatch = useDispatch();
+    const [page, setPage] = useState(0);
+    const [fetching, setFetching] = useState(true);
+
+    const scrollHandler = (e) => {
+        if (
+            e.target.documentElement.scrollHeight -
+                (e.target.documentElement.scrollTop + window.innerHeight) <
+            120
+        ) {
+            setFetching(true);
+            setPage(page + 1);
+        }
+    };
 
     useEffect(() => {
         dispatch(fetchCats());
-	}, [dispatch]);
-	
+    }, [dispatch]);
+
+    useEffect(() => {
+        document.addEventListener("scroll", scrollHandler);
+        return () => {
+            document.removeEventListener("scroll", scrollHandler);
+        };
+    });
+
+    useEffect(() => {
+        if (fetching) {
+            //debouncer
+            setTimeout(() => {
+                dispatch(loadMore(page));
+            }, 1000);
+            setFetching(false);
+        }
+    }, [dispatch, fetching, page]);
+
     return (
         <Routes>
             <Route path="/" element={<AppWrapper />}>
--- src/store/reducers/fetchCatsReducer.js
+++ src/store/reducers/fetchCatsReducer.js
@@ -8,11 +8,19 @@ export const fetchCats = createAsyncThunk("cats/fetchCats", async () => {
     return await response.json();
 });
 
+export const loadMore = createAsyncThunk("cats/loadMore", async (page) => {
+    const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?limit=10&page=${page}&mime_types=jpg,pn&api_key=${api_key}`
+    );
+    return await response.json();
+});
+
 const catsSlice = createSlice({
     name: "cats",
     initialState: {
         loading: false,
         cats: [],
+        newCats: [],
         favorites: [],
     },
     reducers: {
@@ -25,9 +33,7 @@ const catsSlice = createSlice({
             state.favorites.push(cat);
         },
         removeFromFavorite(state, action) {
-            const cat = state.cats.find(
-                (cat) => cat.id === action.payload
-            );
+            const cat = state.cats.find((cat) => cat.id === action.payload);
             cat.liked = false;
             state.favorites = state.favorites.filter(
                 (cat) => cat.id !== action.payload
@@ -45,6 +51,15 @@ const catsSlice = createSlice({
                 return cat;
             });
             state.loading = false;
+        },
+        [loadMore.fulfilled]: (state, action) => {
+            state.newCats = action.payload;
+            state.newCats = state.newCats.map((cat) => {
+                cat.liked = false;
+                return cat;
+            });
+            console.log(state.newCats);
+            state.cats = state.cats.concat(state.newCats);
         },
     },
 });
