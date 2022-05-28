7419bbc568293c26d8768e59eb603b6cb1d3c767
jwesa <layoshicity@gmail.com>
2022-05-28 15:30:45 +0300

    Removed folder AllCatsPage

--- src/components/AllCatsPage/AllCatsPage.js
+++
@@ -1,9 +0,0 @@
-import "./AllCatsPage.scss";
-import { useSelector } from "react-redux";
-
-const AllCatsPage = () => {
-    const { loading } = useSelector((state) => state.cats);
-    return <div>AllCatsPage</div>;
-};
-
-export default AllCatsPage;
