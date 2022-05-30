864e00174c3ea8fba972b09f22093481a780e3c1
jwesa <layoshicity@gmail.com>
2022-05-30 19:12:05 +0300

    Scroll to top after clicking favorite cats

--- src/components/FavoriteCats/FavoriteCats.js
+++ src/components/FavoriteCats/FavoriteCats.js
@@ -1,12 +1,17 @@
 import CatCard from "../CatCard/CatCard";
 import CardsContainer from "../../containers/CardsContainer/CardsContainer";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 import { removeFromFavorite } from "../../store/reducers/fetchCatsReducer";
 
 const FavoriteCats = () => {
     const dispatch = useDispatch();
     const { favorites } = useSelector((state) => state.cats);
-	
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <CardsContainer>
             {favorites.map((item) => {
